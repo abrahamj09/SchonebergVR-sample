a5eb8d3f74bafa25629094b5ad280cda897e67bd
abrahamj09 <abrahamjms09@gmail.com>
2025-11-22 18:47:43 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2main",
+      "name": "2main",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5759909047194327,
+        "pitch": 0.5638949096102408,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20038746690004317,
+          "pitch": 1.089415459912317,
+          "rotation": 3.141592653589793,
+          "target": "2-2main_top"
+        },
+        {
+          "yaw": -1.2029069491493605,
+          "pitch": 0.10467392374510531,
+          "rotation": 0,
+          "target": "1-2buffer1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3134781940114753,
+          "pitch": 0.4314256095336475,
+          "title": "Schoneberg Tour Sample",
+          "text": "This is a trial created in Marzipano and hosted by Github."
+        }
+      ]
+    },
+    {
+      "id": "1-2buffer1",
+      "name": "2buffer1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5840140189365695,
+        "pitch": 0.1985135983075459,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6671031224954689,
+          "pitch": 0.15880601190881904,
+          "rotation": 0,
+          "target": "0-2main"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2main_top",
+      "name": "2main_top",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.1221730476396079,
+        "pitch": 0.8348702732560156,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14716983577873677,
+          "pitch": 1.0262108221196176,
+          "rotation": 0,
+          "target": "0-2main"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
